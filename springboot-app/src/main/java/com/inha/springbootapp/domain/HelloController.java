3ab555e8bb02c8b705d8ad52f3fd89ea778c757f
정재현 <chwogus0303@naver.com>
2024-10-15 01:07:14 +0900

    refactor(springboot-app): MARIADB 제거

--- springboot-app/src/main/java/com/inha/springbootapp/domain/HelloController.java
+++ springboot-app/src/main/java/com/inha/springbootapp/domain/HelloController.java
@@ -6,12 +6,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import io.github.cdimascio.dotenv.Dotenv;
 
-import javax.sql.DataSource;
-import org.springframework.beans.factory.annotation.Autowired;
+// import javax.sql.DataSource;
+// import org.springframework.beans.factory.annotation.Autowired;
 
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.SQLException;
+// import java.sql.Connection;
+// import java.sql.DatabaseMetaData;
+// import java.sql.SQLException;
 
 // 컨트롤러에서 CORS를 개별적으로 설정하는 방법
 // @CrossOrigin(origins = "http://localhost:3001") // React 애플리케이션의 URL
@@ -22,19 +22,19 @@ public class HelloController {
     Dotenv dotenv = Dotenv.load();
     private final String reactURL = "http://localhost:" + dotenv.get("REACT_HOST_PORT");
 
-    @Autowired
-    private DataSource dataSource; // MariaDB와 MySQL은 동일한 JDBC 드라이버를 사용하기 때문에, 하나의 DataSource를 통해 두 데이터베이스에 연결할 수 있다.
+    // @Autowired
+    // private DataSource dataSource; // MariaDB와 MySQL은 동일한 JDBC 드라이버를 사용하기 때문에, 하나의 DataSource를 통해 두 데이터베이스에 연결할 수 있다.
 
-    @GetMapping("/health/RDBMS")
-    public String pingRDBMS() {
-        try (Connection connection = dataSource.getConnection()) {
-            DatabaseMetaData metaData = connection.getMetaData();   // IP주소와 포트를 가져오기 위한 메타데이터
-            String url = metaData.getURL();                         // JDBC URL에서 IP 주소와 포트를 얻기
-            return "RDBMS: Connected - " + url;
-        } catch (SQLException e) {
-            return "RDBMS: Not Connected - " + e.getMessage();
-        }
-    }
+    // @GetMapping("/health/RDBMS")
+    // public String pingRDBMS() {
+    //     try (Connection connection = dataSource.getConnection()) {
+    //         DatabaseMetaData metaData = connection.getMetaData();   // IP주소와 포트를 가져오기 위한 메타데이터
+    //         String url = metaData.getURL();                         // JDBC URL에서 IP 주소와 포트를 얻기
+    //         return "RDBMS: Connected - " + url;
+    //     } catch (SQLException e) {
+    //         return "RDBMS: Not Connected - " + e.getMessage();
+    //     }
+    // }
 
     @GetMapping("/hello")
     public String sayHello() {
