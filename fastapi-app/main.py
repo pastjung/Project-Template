8b0819332ddc607de7c43b7af07ddabb4ac295ae
정재현 <chwogus0303@naver.com>
2024-10-15 23:59:41 +0900

    feat(fastapi-app): Middleware 설정 -> CORS 설정

--- fastapi-app/main.py
+++ fastapi-app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 
 from core.docs import *
+from starlette.middleware.cors import CORSMiddleware
+from fastapi.middleware.gzip import GZipMiddleware
 
 def get_server():
     server = FastAPI(
@@ -9,6 +11,16 @@ def get_server():
         version="1.0.0",
         description=root_description,
         openapi_url="/openapi.json"
+    )
+    server.add_middleware(
+        CORSMiddleware,
+        allow_origins=['*'],
+        allow_credentials=True,
+        allow_methods=['*'],
+        allow_headers=['*'],
+    )
+    server.add_middleware(
+        GZipMiddleware, minimum_size=1000
     )
 
     return server
